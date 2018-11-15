3562792dffef3d10eab235dabf4359c0bc1f471a
Ilya Radchenko <ilya@burstcreations.com>
2018-11-15 12:30:07 -0500

    fix: try initial values

--- addon/components/grid-editor-item/component.js
+++ addon/components/grid-editor-item/component.js
@@ -8,27 +8,42 @@ export default Component.extend({
   layout,
   tagName: '',
 
+  init() {
+    this._super(...arguments);
+
+    this.gridColumnStart = 'auto';
+    this.gridColumnEnd = 'auto';
+    this.gridRowStart = 'auto';
+    this.gridRowEnd = 'auto';
+    this.initial = true;
+  },
+
   style: computed('gridRowStart', 'gridRowEnd', 'gridColumnStart', 'gridColumnEnd', {
     get() {
-      let all = [];
+      let itemIndex = this.itemIndex;
+      //let numElementsPerRow = Math.floor(columns / 1);
+     // let position = itemIndex + 1;
+      let row = Math.floor((itemIndex + 1) / columns);
+      let col = itemIndex % columns;
+      let gridColumnStart = this.gridColumnStart;
+      let gridColumnEnd = this.gridColumnEnd;
+      let gridRowStart = this.gridRowStart;
+      let gridRowEnd = this.gridRowEnd;
+      let initial = this.initial;
 
-      if (this.gridColumnStart) {
-        all.push(`grid-column-start: ${this.gridColumnStart};`);
+      if (!initial) {
+        gridColumnStart = gridColumnStart !== 'auto' ? gridColumnStart : col + 1;
+        gridColumnEnd = gridColumnEnd !== 'auto' ? gridColumnEnd : col + 2;
+        gridRowStart = gridRowStart !== 'auto' ? gridRowStart : row + 1;
+        gridRowEnd = gridRowEnd !== 'auto' ? gridRowEnd : row + 2;
       }
 
-      if (this.gridColumnEnd) {
-        all.push(`grid-column-end: ${this.gridColumnEnd};`);
-      }
-
-      if (this.gridRowStart) {
-        all.push(`grid-row-start: ${this.gridRowStart};`);
-      }
-
-      if (this.gridRowEnd) {
-        all.push(`grid-row-end: ${this.gridRowEnd};`);
-      }
-
-      return all.join('\n');
+      return `
+        grid-column-start: ${gridColumnStart};
+        grid-column-end: ${gridColumnEnd};
+        grid-row-start: ${gridRowStart};
+        grid-row-end: ${gridRowEnd};
+      `;
     }
   }),
 
@@ -37,31 +52,34 @@ export default Component.extend({
       let gridColumnEnd = this.gridColumnEnd;
       let itemIndex = this.itemIndex;
 
-      if (!gridColumnEnd) {
+      if (gridColumnEnd === 'auto') {
         gridColumnEnd = itemIndex + 3;
       } else {
         gridColumnEnd = gridColumnEnd + 1;
       }
       
+      this.set('initial', false);
       this.set('gridColumnStart', itemIndex + 1);
       this.set('gridColumnEnd', gridColumnEnd);
     },
 
     down() {
       let gridRowEnd = this.gridRowEnd;
-      let gridRowStart = this.gridRowStart;
+      //let gridRowStart = this.gridRowStart;
       let itemIndex = this.itemIndex;
-      let itemWidth = gridRowEnd === undefined ? 1 : gridRowEnd - gridRowStart;
-      let numElementsPerRow = Math.floor(columns / itemWidth);
-      let row = Math.ceil(numElementsPerRow / columns);
+      //let itemWidth = gridRowEnd === undefined ? 1 : gridRowEnd - gridRowStart;
+      //let numElementsPerRow = Math.floor(columns / itemWidth);
+      //let row = Math.ceil(numElementsPerRow / columns);
+      let row = Math.floor(itemIndex / columns);
 
-      if (!gridRowEnd) {
-        gridRowEnd = row + 2;
+      if (gridRowEnd === 'auto') {
+        gridRowEnd = row + 3;
       } else {
         gridRowEnd = gridRowEnd + 1;
       }
       
-      this.set('gridRowStart', itemIndex + 1);
+      this.set('initial', false);
+      this.set('gridRowStart', row + 1);
       this.set('gridRowEnd', gridRowEnd);
     }
   }
--- tests/dummy/app/application/controller.js
+++ tests/dummy/app/application/controller.js
@@ -11,6 +11,8 @@ export default Controller.extend({
       { name: 'd' },
       { name: 'e' },
       { name: 'f' },
+      { name: 'g' },
+      { name: 'h' },
     ]);
   }
 });
